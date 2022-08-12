166c4b523c31e947ab9c7e79c27f57804f2f5180
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-08-12 20:52:11 +0200

    First commit

---
+++ controllers/tourController.js
@@ -0,0 +1,49 @@
+const Tour = require('../models/tourModel');
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    reqestedAt: req.requestTime,
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
+};
+
+exports.createTour = (req, res) => {
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
