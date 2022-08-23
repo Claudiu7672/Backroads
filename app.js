fa4170fbb37474931f9ccb951bfb9f2b85e5752d
Claudiu Florin Popa <claudiup_15@yahoo.com>
2022-08-23 18:31:22 +0200

    Set up pug and create the base.pug file

--- app.js
+++ app.js
@@ -62,12 +62,32 @@ app.use(
   })
 );
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 //Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Claudiu',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
